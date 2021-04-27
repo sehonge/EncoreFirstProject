c2a56b547113a87be4c844da2c6d29834e668f60
Se-Hong Im <sehong1602@naver.com>
2021-04-27 15:22:39 +0900

    change class path java jre-1.8

--- project/src/model/CustomerDaoImpl.java
+++ project/src/model/CustomerDaoImpl.java
@@ -1,12 +1,14 @@
 package model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import config.ServerInfo;
 import util.DataSourceManager;
 
 public class CustomerDaoImpl implements CustomerDao{
@@ -16,23 +18,25 @@ public class CustomerDaoImpl implements CustomerDao{
 	private static CustomerDaoImpl dao = new CustomerDaoImpl();
 	
 	private CustomerDaoImpl() {
-		ds = DataSourceManager.getInstance().getConnection();
+		try {
+			Class.forName(ServerInfo.DRIVER_NAME);
+			System.out.println("드라이버 로딩 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.print("드라이버 로딩 실패 ");
+			System.out.println(e);
+		}
+		//ds = DataSourceManager.getInstance().getConnection();
 	}
 	
 	public static CustomerDaoImpl getInstance() {
 		return dao;
 	}	
 
-	@Override
-	public boolean matchIdPassword(String id, String password) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 	public Connection getConnection() throws SQLException {
 		System.out.println("디비연결...");
 		
-		return ds.getConnection();
+		return DriverManager.getConnection(ServerInfo.URL, ServerInfo.USER, ServerInfo.PASS);
+		//return ds.getConnection();
 	}
 	
 	public void closeAll(PreparedStatement ps, Connection conn) throws SQLException{
@@ -47,18 +51,66 @@ public class CustomerDaoImpl implements CustomerDao{
 		return;
 	}	
 	
+	
+	@Override
+	public boolean matchIdPassword(String id, String password) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
 	@Override
 	public boolean SignUp(String id, String password, String name, String address, String phoneNumber, String email)
 			throws SQLException {
+		boolean ret = false;
+		Connection conn = null;
+		PreparedStatement ps = null;
 		
+		try {
+			conn = getConnection();
+			String query = "INSERT INTO customer VALUES(?,?,?,?,?,?,?)";
+			ps = conn.prepareStatement(query);
+			
+			ps.setString(1, id);
+			ps.setString(2, password);
+			ps.setString(3, name);
+			ps.setString(4, address);
+			ps.setString(5, phoneNumber);
+			ps.setString(6, email);
+			ps.setInt(7, 0);
+			
+			
+		} finally {
+			closeAll(ps, conn);
+		}
 		
 		return false;
 	}
 
 	@Override
 	public String findId(String custEmail) throws SQLException {
+		String ret = "";
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		
-		return null;
+		try {
+			conn = getConnection();
+			String query = "SELECT * FROM customer WHERE CUST_EMAIL=?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, custEmail);
+			
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				ret = rs.getString(1);
+			}
+			
+		} finally {
+			closeAll(rs, ps, conn);
+		}
+		
+		return ret;
 	}
 
 	@Override
@@ -79,8 +131,10 @@ public class CustomerDaoImpl implements CustomerDao{
 		return false;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
+		
+		CustomerDaoImpl test = new CustomerDaoImpl();
+		System.out.println(test.findId("encore@gmail.com"));
 
 	}
 
