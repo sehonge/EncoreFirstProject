0cb50fb6f10568aafcd94915c4df8e3b6d6187e6
Se-Hong Im <sehong1602@naver.com>
2021-04-29 00:19:13 +0900

    change customerDaoImpl(signIn)

--- project/src/model/CustomerDao.java
+++ project/src/model/CustomerDao.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public interface CustomerDao {
 	
-	boolean matchIdPassword(String id, String password) throws SQLException;
-	boolean SignUp(String id, String password, String name, String address, String phoneNumber, String email) throws SQLException;
+	Customer signIn(String id, String password) throws SQLException;
+	boolean signUp(String id, String password, String name, String address, String phoneNumber, String email) throws SQLException;
 	String findId(String custEmail) throws SQLException; // email(or 핸드폰번호, 이름)으로 아이디 찾기
 	String findPw(String custId) throws SQLException; // id(or 이름, email, 핸드폰번호)로 패스워드 찾기
 	Customer showCustomer(String custId) throws SQLException; // id에 해당하는 customer 반환
