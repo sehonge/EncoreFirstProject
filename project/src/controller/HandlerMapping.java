9320494b55257796669c5135424f612e7dbe0bc2
dudtn6230 <dudtn2330@gmail.com>
2021-04-28 21:01:36 +0900

    LoginController추가, HandlerMapping 수정

--- project/src/controller/HandlerMapping.java
+++ project/src/controller/HandlerMapping.java
@@ -22,6 +22,8 @@ public class HandlerMapping {
 			controller = new SpendMoneyController();
 		}else if(command.equals("makeOrder.do")) {
 			controller = new MakeOrderController();
+		}else if(command.equals("login.do")) {
+			controller = new LoginController();
 		}
 		
 		
