26dd88d055a01b407e9e51ea6980ba7fa8ed20c4
dudtn6230 <dudtn2330@gmail.com>
2021-04-29 00:41:05 +0900

    commit

--- project/src/controller/LoginController.java
+++ project/src/controller/LoginController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import model.Customer;
 import model.CustomerDaoImpl;
 
@@ -7,21 +13,28 @@ public class LoginController implements Controller{
 
 		@Override
 		public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+			
 			String id = request.getParameter("id");
 			String password = request.getParameter("password");
 			String path = "";
+			String login = "login.jsp";
+			String err = "errmsg.jsp";
 			
 			try {
-				Customer rvo = CustomerDaoImpl.getInstance().matchIdPassword(id, password);
+				Customer rvo = CustomerDaoImpl.getInstance().showCustomer(id);
 				HttpSession session =request.getSession();
-				if(rvo != null) {
+				if(CustomerDaoImpl.getInstance().matchIdPassword(id,password)) {
 					
-					session.setAttribute("vo",rvo);
-					path = "Index.jsp"
-				}
-			}catch(SQLException e) {
-				
-			}
+					session.setAttribute("vo", rvo); //통신이 끊기기 전 남는 애들
+					path = "Index.jsp";
+				}else {
+					request.setAttribute("errmsg.jsp", err); //단발성 애들
+					path = "login.jsp";
+	            }
+	        } catch (SQLException e) {
+	        	request.setAttribute("errmsg.jsp", err);
+	        	path = "login.jsp";
+	        }
 			return new ModelAndView(path);
 		}
-	}
+}
